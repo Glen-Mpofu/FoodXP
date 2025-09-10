99671ae3a4c74b172579fdd1a02e108290f5bc59
Tshepo <mgthades2004@gmail.com>
2025-09-10 12:11:07 +0200

    working on the camera but it doesnt want to work

--- app/dashboard/camerascreen.jsx
+++ app/dashboard/camerascreen.jsx
@@ -1,14 +1,54 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
+import React, { useState, useEffect, useRef } from 'react';
 
-const camerascreen = () => {
+const CameraScreen = () => {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [hasMediaPermission, setHasMediaPermission] = useState(null);
+  const [type, setType] = useState('back');
+  const [flash, setFlash] = useState('off');
+  const cameraRef = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status: cameraStatus } = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus === 'granted');
+
+      const { status: mediaStatus } = await MediaLibrary.requestPermissionsAsync();
+      setHasMediaPermission(mediaStatus === 'granted');
+    })();
+  }, []);
+
+  if (hasCameraPermission === null || hasMediaPermission === null) {
+    // Permissions are still loading
+    return <View><Text>Loading permissions...</Text></View>;
+  }
+
+  if (!hasCameraPermission) {
+    return <View><Text>No access to camera</Text></View>;
+  }
+
+  if (!hasMediaPermission) {
+    return <View><Text>No access to media library</Text></View>;
+  }
+
   return (
-    <View>
-      <Text>camerascreen</Text>
+    <View style={{ flex: 1 }}>
+      <Camera
+          style = {styles.camera}
+          type = {type}
+          ref = {cameraRef}
+      />
+
     </View>
-  )
-}
+  );
+};
 
-export default camerascreen
+export default CameraScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  camera: {
+
+  }
+});
